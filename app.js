d2a057bee614e31b9e0da8d2e44529c8123af653
Gourav Hammad <51832241+gouravhammad@users.noreply.github.com>
2020-05-06 11:51:22 +0530

    Init Commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var chalk = require('chalk')
+
+// Blue color 
+console.log(chalk.black.bgYellow('Greetings from Geekforgeeks'));
+
+// Blue background and black text 
+console.log(chalk.black.bgBlue('Greetings from Geekforgeeks'));
+
+// Modifiers can also be applied
+console.log(chalk.bold.green('Greetings from Geekforgeeks'));
